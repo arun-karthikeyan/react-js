2bec353b4db3caf43542257600e77ae70d75ce85
Arun Karthikeyan <akarthi2@asu.edu>
2016-08-07 19:58:12 -0700

    resuable components update

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,22 +1,36 @@
 var Button = React.createClass({
-  getInitialState() {
-    return {
-      counter: 0
-    };
+  localHandleClick() {
+    this.props.localHandleClick(this.props.increment);
   },
-  handleClick() {
+  render() {return < button onClick = {this.localHandleClick} > {this.props.increment} < /button>;}
+});
+
+var Result = React.createClass({
+  render() {
+    return <div>{this.props.localCounter}</div>
+  }
+});
+
+var Main = React.createClass({
+  getInitialState(){return {counter: 0};},
+  handleClick(increment){
     this.setState(previousState => {
       return {
-        counter: previousState.counter + 1
+        counter: previousState.counter+increment
       };
-    });
+    })
   },
-  render() {
-    return ( < button onClick = {
-      this.handleClick
-    } > {
-      this.state.counter
-    } < /button>);}
-  });
+  render(){
+    return (
+      <div>
+      <Button localHandleClick={this.handleClick} increment={1}/>
+      <Button localHandleClick={this.handleClick} increment={2}/>
+      <Button localHandleClick={this.handleClick} increment={5}/>
+      <Button localHandleClick={this.handleClick} increment={10}/>
+      <Result localCounter={this.state.counter}/>
+      </div>
+    );
+  }
+});
 
-  ReactDOM.render( < Button / > , document.getElementById("root"));
+ReactDOM.render( <Main / > , document.getElementById("root"));
