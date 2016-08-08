f83eccdafb3622bedbc1f6a10efbe2f2cf1ecfe8
Arun Karthikeyan <akarthi2@asu.edu>
2016-08-07 18:58:40 -0700

    updated js code to es2015

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,7 +1,22 @@
 var Button = React.createClass({
-getInitialState() {return {counter: 0};},
-handleClick() {this.setState({counter: this.state.counter+1});},
-render() {return (<button onClick={this.handleClick}>{this.state.counter}</button>);}
-});
+  getInitialState() {
+    return {
+      counter: 0
+    };
+  },
+  handleClick() {
+    this.setState(previousState => {
+      return {
+        counter: previousState.counter + 1
+      };
+    });
+  },
+  render() {
+    return ( < button onClick = {
+      this.handleClick
+    } > {
+      this.state.counter
+    } < /button>);}
+  });
 
-ReactDOM.render(<Button />, document.getElementById("root"));
+  ReactDOM.render( < Button / > , document.getElementById("root"));
