89972105f300bba4242b26296e2a12df591b041e
Arun Karthikeyan <akarthi2@asu.edu>
2016-08-07 18:20:16 -0700

    react base + my first react example

---
+++ scripts/myscript.js
@@ -0,0 +1,7 @@
+var Button = React.createClass({
+getInitialState() {return {counter: 0};},
+handleClick() {this.setState({counter: this.state.counter+1});},
+render() {return (<button onClick={this.handleClick}>{this.state.counter}</button>);}
+});
+
+ReactDOM.render(<Button />, document.getElementById("root"));
